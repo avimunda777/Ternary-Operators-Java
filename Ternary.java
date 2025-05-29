7cfdf0f66b315d93a667e494885a6772451fdf6d
Avinash Singh Munda <96285074+avimunda777@users.noreply.github.com>
2025-05-30 00:27:01 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,9 @@
+                                /* Ternary Operator Example...! */
+public class Ternary {
+    public static void main(String[] args) {
+
+        int a = 10, b = 4, c = 2, Max;
+        Max = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println(Max);
+    }
+}
